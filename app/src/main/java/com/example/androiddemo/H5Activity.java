830544dd525ff922f22d1d7dcab4af09d865d1e9
wuketao <wuketao@houbank.com>
2019-07-05 20:25:19 +0800

    update

---
+++ app/src/main/java/com/example/androiddemo/H5Activity.java
@@ -0,0 +1,69 @@
+package com.example.androiddemo;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class H5Activity extends AppCompatActivity {
+
+    WebView mWebview;
+    WebSettings mWebSettings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_h5);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mWebview = (WebView) findViewById(R.id.webView1);
+
+        //步骤2. 选择加载方式
+        //方式1. 加载一个网页：
+        mWebview.loadUrl("http://housekeeping.zui8090.com/");
+
+        mWebview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+
+        //设置WebChromeClient类
+        mWebview.setWebChromeClient(new WebChromeClient() {
+            //获取网站标题
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                System.out.println("标题在这里");
+            }
+            //获取加载进度
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                if (newProgress < 100) {
+                    String progress = newProgress + "%";
+                } else if (newProgress == 100) {
+                    String progress = newProgress + "%";
+                }
+            }
+        });
+    }
+
+}
